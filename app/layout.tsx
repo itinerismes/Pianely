586ac5687783f7dace1cdad5fe06ec1ee5fa6dae
michael <itinerisme@yahoo.fr>
2026-01-07 21:46:33 +0100

    fix: Increase layout margin to lg:ml-32 (128px) to prevent sidebar text overlap

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <DashboardSidebar />
-        <div className="lg:ml-20">
+        <div className="lg:ml-32">
           {children}
         </div>
       </body>
